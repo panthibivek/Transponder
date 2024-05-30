1b6dd7cfa127a9d84c6f7fa1aea0c4b0be93a703
Bivek Panthi <go34wun@mytum.de>
2024-05-31 01:35:23 +0200

    new data gen

--- gen_data_bidirectional.py
+++ gen_data_bidirectional.py
@@ -96,7 +96,8 @@ class GenData:
         backbone_inputs = self.tokenizer(prompt, return_tensors="pt")
         samples_from_each_prompt = (int(backbone_inputs['input_ids'].shape[1])-self.PONDER_CONTEXT_LENGTH-1)//sampling_skip
         for idx in range(0, samples_from_each_prompt, 1):
-            if random_gen_bool(0.1):
+            # if random_gen_bool(0.1):
+            if random_gen_bool(1):
                 if int(backbone_inputs['input_ids'].shape[1]) <= 5:
                     break
                 last_token_last_hidden_state, token_index, masked_token = self.__get_hidden_layer(
