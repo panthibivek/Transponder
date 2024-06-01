58a94419c3a16f79d066440caaaab7571991c587
Bivek Panthi <go34wun@mytum.de>
2024-06-01 17:30:17 +0200

    update

--- gen_data_bidirectional.py
+++ gen_data_bidirectional.py
@@ -58,7 +58,7 @@ class GenData:
         input_prompts = input_prompts[:200]
         print(f"Total prompts: {len(input_prompts)}")
         # for dummy 
-        input_prompts = ["I am Iron man. Am I made of iron? Wait, I think I am.", "List the top 10 tallest mountains in the world and list their locations. List the top 10 tallest mountains in the world and list their locations. List the top 10 tallest mountains in the world and list their locations."]
+        # input_prompts = ["I am Iron man. Am I made of iron? Wait, I think I am.", "List the top 10 tallest mountains in the world and list their locations. List the top 10 tallest mountains in the world and list their locations. List the top 10 tallest mountains in the world and list their locations."]
 
         groundtruth_token_total_list = []
         prompt_tensor = []
@@ -96,8 +96,7 @@ class GenData:
         backbone_inputs = self.tokenizer(prompt, return_tensors="pt")
         samples_from_each_prompt = (int(backbone_inputs['input_ids'].shape[1])-self.PONDER_CONTEXT_LENGTH-1)//sampling_skip
         for idx in range(0, samples_from_each_prompt, 1):
-            # if random_gen_bool(0.1):
-            if random_gen_bool(0.3):
+            if random_gen_bool(0.1):
                 if int(backbone_inputs['input_ids'].shape[1]) <= 5:
                     break
                 last_token_last_hidden_state, token_index, masked_token = self.__get_hidden_layer(
@@ -149,15 +148,15 @@ class GenData:
                 model_out = model(**backbone_inputs, output_hidden_states=True)
                 last_token_last_hidden_state = model_out.hidden_states[-1][:,current_token_pos,:]
 
-        print(f"Prompt: {prompt}")
-        print(f"Returned hidden layers shape: {model_out.hidden_states[-1].shape}")
-        print(f"last_token_last_hidden_state: {last_token_last_hidden_state.shape}")
-        print(f"Masked Token: {masked_token}")
-        print(f"Masked Token idx in vocab: {token_index}")
-        print(f"Masked token reverse (assert): {self.tokenizer.batch_decode([token_index])[0]}")
-        print(f"Current position: {current_token_pos}")
-        print(f"Backbone input: {backbone_inputs}")
-        print()
+        # print(f"Prompt: {prompt}")
+        # print(f"Returned hidden layers shape: {model_out.hidden_states[-1].shape}")
+        # print(f"last_token_last_hidden_state: {last_token_last_hidden_state.shape}")
+        # print(f"Masked Token: {masked_token}")
+        # print(f"Masked Token idx in vocab: {token_index}")
+        # print(f"Masked token reverse (assert): {self.tokenizer.batch_decode([token_index])[0]}")
+        # print(f"Current position: {current_token_pos}")
+        # print(f"Backbone input: {backbone_inputs}")
+        # print()
         return last_token_last_hidden_state, token_index, masked_token
     
 
