50d5411ee14a8846143e542a6ed7e88b17e92fa4
Bivek Panthi <go34wun@mytum.de>
2024-06-20 14:00:35 +0200

    data gen - multi token masking

--- gen_data_benchmarking.py
+++ gen_data_benchmarking.py
@@ -60,6 +60,7 @@ class GenData:
         print(input_df.columns)
         result_df = input_df.text
         input_prompts = list(set(result_df.tolist()))
+        input_prompts = input_prompts[:500]
         # print(len(input_prompts))
         print(f"Total prompts: {len(input_prompts)}")
         # for dummy 
@@ -71,12 +72,14 @@ class GenData:
             print(f"Current prompt number: {idx}")
             # print(f"The prompt: {prompt}")
             try:
-                last_tokens_last_hidden_state_tensor, masked_token_index_tensor, masked_token_list = self.__generate_row_data(prompt, use_gpu_)
+                last_tokens_last_hidden_state_tensor, last_tokens_last_hidden_state_tensor_nobi, masked_token_index_tensor, masked_token_list = self.__generate_row_data(prompt, use_gpu_)
                 try:
                     hidden_state_tensor = torch.cat((hidden_state_tensor, last_tokens_last_hidden_state_tensor), dim=0)
+                    hidden_state_tensor_nobi = torch.cat((hidden_state_tensor_nobi, last_tokens_last_hidden_state_tensor_nobi), dim=0)
                     groundtruth_tensor = torch.cat((groundtruth_tensor, masked_token_index_tensor), dim=0)
                 except:
                     hidden_state_tensor = last_tokens_last_hidden_state_tensor
+                    hidden_state_tensor_nobi = last_tokens_last_hidden_state_tensor_nobi
                     groundtruth_tensor = masked_token_index_tensor
                 groundtruth_token_total_list += masked_token_list
                 # prompt_tensor += updated_prompt_tensor
@@ -88,10 +91,13 @@ class GenData:
         # output_df = pd.DataFrame({'prompt': prompt_tensor, 'groundtruth_token': groundtruth_token_total_list})
         # output_df.to_csv(f"{output_data_path}.csv", index=False)
         os.makedirs(f"{output_data_path}/hidden_state", exist_ok=True)
+        os.makedirs(f"{output_data_path}/hidden_state_nobi", exist_ok=True)
         os.makedirs(f"{output_data_path}/groundtruth", exist_ok=True)
         torch.save(hidden_state_tensor, f"{output_data_path}/hidden_state/hidden_state_{current_idx}.pt")
+        torch.save(hidden_state_tensor_nobi, f"{output_data_path}/hidden_state_nobi/hidden_state_nobi_{current_idx}.pt")
         torch.save(groundtruth_tensor, f"{output_data_path}/groundtruth/groundtruth_index_{current_idx}.pt")
         # print(f"Combined hidden layer shape: {hidden_state_tensor.shape}")
+        # print(f"Combined hidden layer shape nobi: {hidden_state_tensor_nobi.shape}")
         # print(f"Combined groundtruth one hot tensor shape: {groundtruth_tensor.shape}")
         print(f"Total generated samples: {len(groundtruth_tensor)}")
         return len(groundtruth_tensor)
@@ -100,7 +106,7 @@ class GenData:
         masked_token_list = []
         backbone_inputs = self.tokenizer(prompt, return_tensors="pt")
         total_tokens_in_prompt = int(backbone_inputs['input_ids'].shape[1])
-        random_numbers = [random.randint(0, total_tokens_in_prompt-1) for _ in range(int(self.sampling_percent*total_tokens_in_prompt))]
+        random_numbers = [random.randint(1, total_tokens_in_prompt-1) for _ in range(int(self.sampling_percent*total_tokens_in_prompt))]
         token_indexes = []
         masked_token_list = []
 
@@ -111,22 +117,33 @@ class GenData:
             (backbone_inputs['input_ids'])[0][pos] = 0
             (backbone_inputs['attention_mask'])[0][pos] = 0
 
-        last_hidden_state = self.__get_hidden_layer(
+        last_hidden_state, last_hidden_state_nobi = self.__get_hidden_layer(
             prompt = [prompt], 
             original_backbone_inputs = backbone_inputs,
             use_gpu_ = use_gpu_
         )
+        # print(f"Total Hidden layer size:{last_hidden_state.shape}")
+        # print(f"Total Hidden layer size nobi:{last_hidden_state_nobi.shape}")
 
         # new_prompt = self.tokenizer.batch_decode([backbone_inputs['input_ids'][0]])[0]
         for pos in random_numbers:
             last_token_last_hidden_state = last_hidden_state[:,pos,:]
+            last_token_last_hidden_state_nobi = last_hidden_state_nobi[:,pos-1,:]
+
+            # print(f"Each pos Hidden layer size:{last_token_last_hidden_state.shape}")
+            # print(f"Each pos Hidden layer size nobi:{last_token_last_hidden_state_nobi.shape}")
             try:
                 last_tokens_last_hidden_state_tensor = torch.cat((last_tokens_last_hidden_state_tensor, last_token_last_hidden_state), dim=0)
+                last_tokens_last_hidden_state_tensor_nobi = torch.cat((last_tokens_last_hidden_state_tensor_nobi, last_token_last_hidden_state_nobi), dim=0)
             except:
                 last_tokens_last_hidden_state_tensor = last_token_last_hidden_state
+                last_tokens_last_hidden_state_tensor_nobi = last_token_last_hidden_state_nobi
         masked_token_index_tensor = torch.tensor(token_indexes)
 
-        return last_tokens_last_hidden_state_tensor, masked_token_index_tensor, masked_token_list
+        # print(f"All pos in row layer size:{last_tokens_last_hidden_state_tensor.shape}")
+        # print(f"All pos in row layer size nobi:{last_tokens_last_hidden_state_tensor_nobi.shape}")
+
+        return last_tokens_last_hidden_state_tensor, last_tokens_last_hidden_state_tensor_nobi, masked_token_index_tensor, masked_token_list
 
     def __get_hidden_layer(self, prompt: list, original_backbone_inputs = None, use_gpu_:bool=False):
         backbone_inputs = copy.deepcopy(original_backbone_inputs)
@@ -149,13 +166,29 @@ class GenData:
                     del last_token_last_hidden_state_gpu
                     gc.collect()
                     torch.cuda.empty_cache()
+                
+                # without bidirectional switch
+                model_out_nobi = model_cuda(**backbone_inputs, output_hidden_states=True)
+                # last_token_last_hidden_state_gpu = model_out.hidden_states[-1][:,current_token_pos,:]
+                last_token_last_hidden_state_gpu_nobi = model_out_nobi.hidden_states[-1]
+                last_token_last_hidden_state_nobi = last_token_last_hidden_state_gpu_nobi.to('cpu')
+
+                del model_out_nobi
+                del last_token_last_hidden_state_gpu_nobi
+                gc.collect()
+                torch.cuda.empty_cache()
+
         else:
             with LlamaBidirectionalSwitch(model):
                 model_out = model(**backbone_inputs, output_hidden_states=True)
                 last_token_last_hidden_state = model_out.hidden_states[-1]
                 # last_token_last_hidden_state = model_out.hidden_states[-1][:,current_token_pos,:]
+            
+            # without bidirectional switch
+            model_out_nobi = model(**backbone_inputs, output_hidden_states=True)
+            last_token_last_hidden_state_nobi = model_out_nobi.hidden_states[-1]
 
-        return last_token_last_hidden_state
+        return last_token_last_hidden_state, last_token_last_hidden_state_nobi
     
 
 # Additional functions
@@ -183,13 +216,13 @@ def random_gen_bool(prob_true=0.2):
     return random.random() < prob_true
 
 if __name__=="__main__":
-    input_data = "data/input_data.csv"
-    log_file = "data/log.txt"
-    os.makedirs("data", exist_ok=True)
-    output_data_path = "data"
+    input_data = "data_benchmarking/input_data.csv"
+    log_file = "data_benchmarking/log.txt"
+    os.makedirs("data_benchmarking", exist_ok=True)
+    output_data_path = "data_benchmarking"
     gen_obj = GenData()
 
-    for idx in range(2000):
+    for idx in range(1):
         print("Getting the input data.")
         url = f"https://huggingface.co/datasets/cerebras/SlimPajama-627B/resolve/main/train/chunk1/example_train_{idx}.jsonl.zst"
         with open(log_file, 'a+') as file:
